bcf21b980d05edcde203dd4e79b813e32dd5a9b3
Daniel-615 <suyanangel615@gmail.com>
2026-05-07 00:12:05 -0600

    feat: added fullscreen, windowscreen

--- Scripts/Menu/resolution.gd
+++ Scripts/Menu/resolution.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var display_options = $VBoxContainer/Resolution
+
 
 func _on_atras_pressed() -> void:
 	get_tree().change_scene_to_file(
@@ -8,39 +10,4 @@ func _on_atras_pressed() -> void:
 
 
 func _on_resolution_item_selected(index: int) -> void:
-	match index:
-		
-		# Ventana
-		0:
-			DisplayServer.window_set_flag(
-				DisplayServer.WINDOW_FLAG_BORDERLESS,
-				false
-			)
-			
-			DisplayServer.window_set_mode(
-				DisplayServer.WINDOW_MODE_WINDOWED
-			)
-		
-		
-		# Pantalla completa
-		1:
-			DisplayServer.window_set_flag(
-				DisplayServer.WINDOW_FLAG_BORDERLESS,
-				false
-			)
-			
-			DisplayServer.window_set_mode(
-				DisplayServer.WINDOW_MODE_FULLSCREEN
-			)
-		
-		
-		# Pantalla completa sin bordes
-		2:
-			DisplayServer.window_set_flag(
-				DisplayServer.WINDOW_FLAG_BORDERLESS,
-				true
-			)
-			
-			DisplayServer.window_set_mode(
-				DisplayServer.WINDOW_MODE_FULLSCREEN
-			)
+	Config.change_displayMode(index)
--- Scripts/config.gd
+++ Scripts/config.gd
@@ -1,4 +1,19 @@
 extends Node
 
-var metodo_busqueda := "A"  
+var metodo_busqueda := "A"
 var juego_iniciado := false
+
+
+func change_displayMode(index):
+
+	var window = get_window()
+
+	match index:
+
+		0:
+
+			window.mode = Window.MODE_WINDOWED
+
+		1:
+
+			window.mode = Window.MODE_FULLSCREEN
